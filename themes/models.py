a2744e382fbf58b81c7a00e14fa4c63a6e9e40e4
Eyob Tariku <eyobtariku@gmail.com>
2020-09-13 23:42:41 +0300

    Add unique slug field to themes `Category` model

--- themes/models.py
+++ themes/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Category(models.Model):
     """Bootstrap theme category."""
     title = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(blank=True)
 
     class Meta:
@@ -62,6 +63,7 @@ class Theme(models.Model):
         null=True, blank=True,
         help_text='Archived theme file.'
     )
+    preview_url = models.URLField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
