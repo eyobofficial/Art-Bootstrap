94d999af1dd3bf8de739949c38a456c0aa0fec90
Eyob Tariku <eyobtariku@gmail.com>
2021-03-13 04:10:14 +0300

    Add facebook open graph & twitter tags for SEO

--- config/settings/base.py
+++ config/settings/base.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
